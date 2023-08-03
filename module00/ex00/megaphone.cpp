4d306903040a5d448a11ad3251ea773e54569ffe
Marouane Fouadi <procompte97@gmail.com>
2023-08-03 08:50:37 +0100

    [Finished] ex00 Megaphone

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+/*
+*    $>./megaphone "shhhhh... I think the students are asleep..."
+*       SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
+*    $>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+*       DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+*    $>./megaphone
+*       * LOUD AND UNBEARABLE FEEDBACK NOISE *
+*    $>
+*/
 
-int main()
+#include <iostream>
+#include <cstring>
+#include <locale>
+
+int main(int ac, char **av)
 {
-    std::cout << "Hey World\n";
+    if (ac > 1){
+    std::locale loc; // object initialized with the default locale.
+        int i = -1;
+        av++;
+        while (av[++i]){
+            std::string str=av[i];
+            for (std::string::size_type i=0; i<str.length(); ++i) //  typedef size_t size_type;
+                std::cout << std::toupper(str[i], loc);
+        }
+        std::cout << "\n";
+        return (0);
+    }
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
     return (0);
 }
