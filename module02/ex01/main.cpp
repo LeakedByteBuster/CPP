2f101b371d28cf633efa7a9a45dd1705ab9072be
Marouane Fouadi <procompte97@gmail.com>
2023-09-01 08:28:02 +0100

    [DOC] removed printBits() && Added FixedPoint binary to gitignore

--- module02/ex01/main.cpp
+++ module02/ex01/main.cpp
@@ -1,32 +1,19 @@
 /*
-*   IEE 754 Multiplaction: s ==> sign | e ==> exponent | 'm' => mantissa 
-*                               | n ==> number of bits for fractional part
+*   IEE 754 : s ==> sign | e ==> exponent | 'm' => mantissa 
+*                             | n ==> number of bits for fractional part
 *   
-*   1.  Converting from floating point to FixedPoint
-*           [ Formula (m1 * m2) * 2 ^ (e1 + e2) ]
-*   2.  round the result before storing it the fixed point
-*   3.  convert your fixed point representation to float by:
+*   1.  Multiplication of floating numbers
+*           [ Formula (m1 * m2) * (2 ^ (e1 + e2)) ]
+*   2.  Multiply the given float or int number by (2 ^ n)
+*   3.  round the result before storing it the fixed point
+*   4.  convert your fixed point representation to float by:
 *           [  FixedPointRepresentation / 2^n ]
+*   5. To 
 *   Note: std::cout its precision is up 5
 */
 
 #include <iostream>
 #include "Fixed.hpp"
-
-void    printBinary()
-{
-    int  x = 1;
-    int  n = sizeof(int) * 8 -1;
-
-    n = sizeof(int) * 8 -1;
-    for (; n >= 0;n--){
-        std::cout << ((x >> n) & 1);
-        if (n%8 == 0)
-            printf(" ");
-    }
-    std::cout << "\n";
-    return ;
-}
 
 int main()
 {
@@ -45,10 +32,6 @@ int main()
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-
-    #ifndef NDEBUG
-        printBinary();
-    #endif // NDEBUG
 
     return 0;
 }
